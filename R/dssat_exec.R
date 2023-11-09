eca7b176c593f0faea64fcfb4e61800b5f0bbffd
rhijmans <r.hijmans@gmail.com>
2023-11-09 16:10:51 +0100

    d

--- R/dssat_exec.R
+++ R/dssat_exec.R
@@ -1,6 +1,7 @@
 
-rundssat <-function(i,path.to.extdata,TRT,AOI) {
-	setwd(paste(path.to.extdata,paste0('EXTE', formatC(width = 4, as.integer((i)), flag = "0")), sep = "/"))
+rundssat <-function(i, path.to.extdata, TRT) {
+	f <- paste0('EXTE', formatC(width = 4, as.integer(i), flag = "0"))
+	setwd(file.path(path.to.extdata, f))
 	# Generate a DSSAT batch file using a tibble
 	options(DSSAT.CSM="/opt/DSSAT/v4.8.1.40/dscsm048")
 	tib <- tibble(FILEX=paste0('EXTE', formatC(width = 4, as.integer((i)), flag = "0"),'.SBX'), TRTNO=TRT, RP=1, SQ=0, OP=0, CO=0)
@@ -8,27 +9,19 @@ rundssat <-function(i,path.to.extdata,TRT,AOI) {
 	# Run DSSAT-CSM
 	DSSAT::run_dssat(file_name="DSSBatch.v48",suppress_output = TRUE)
 	# Change output file name
-	file.rename("Summary.OUT", paste0(path.to.extdata, '/', 'EXTE', formatC(width = 4, as.integer((i)), flag = "0"), '/', 'EXTE', formatC(width = 4, as.integer((i)), flag = "0"), '.OUT'))
+	file.rename("Summary.OUT", paste0(path.to.extdata, '/', 'EXTE', f, '/', f, '.OUT'))
 }
 
 
-dssat.exec <- function(country, useCaseName, Crop, AOI = FALSE,TRT=1, cultivarType= "mediumDuration", 
-	path="/home/jovyan/agwise-potentialyield/dataops/potentialyield/Data/") {	
+dssat.exec <- function(TRT=1, path) {	
 	
-	#require(doParallel)
-	#require(foreach)
-	# Set number of parallel workers
-	#cls <- parallel::makePSOCKcluster(jobs)
-	#doParallel::registerDoParallel(cls)
-	#Set working directory to save the results
-	path.to.extdata <- file.path(path, paste0("useCase_", country, "_", useCaseName, "/", Crop, "/transform/DSSAT"))
+	path.to.extdata <- file.path(path, "/transform/DSSAT")
 	setwd(path.to.extdata)	
 	folders <- list.dirs(".", full.names = FALSE, recursive = FALSE)
 	matching_folders <- folders[grepl("EXTE", folders, ignore.case = TRUE)]
 	#foreach::foreach(i=seq_along(matching_folders), .export = '.GlobalEnv', .inorder = TRUE, .packages = c("tidyverse", "DSSAT")) %dopar% {
  
-	print("Progress:")
-	results <- purrr::map(seq_along(matching_folders), rundssat, path.to.extdata=path.to.extdata, TRT=TRT, AOI=AOI) 
+	results <- purrr::map(seq_along(matching_folders), rundssat, path.to.extdata=path.to.extdata, TRT=TRT) 
 	# %||% print("Progress:")
 }
 
--- R/merge_DSSAT_output.R
+++ R/merge_DSSAT_output.R
@@ -1,28 +1,19 @@
 
-merge_DSSAT_output <- function(country, useCaseName,Crop, AOI=FALSE,season=NULL) {
+merge_DSSAT_output <- function(path) {
   
   # Set number of parallel workers
 	#cls <- parallel::makePSOCKcluster(jobs)
 	#doParallel::registerDoParallel(cls)
-	if (AOI) {
-		if (is.null(season)) {
-			print("with AOI=TRUE, season can not be null; provide season number")
-			return(NULL)
-		}
-		path.to.extdata <- paste("/home/jovyan/agwise-potentialyield/dataops/potentialyield/Data/useCase_", country, "_",useCaseName, "/", Crop, "/transform/DSSAT/AOI", sep="")
-	} else {
-		path.to.extdata <- paste("/home/jovyan/agwise-potentialyield/dataops/potentialyield/Data/useCase_", country, "_",useCaseName, "/", Crop, "/transform/DSSAT", sep="")
-	}
+
+	path.to.extdata <- file.path(path, "/transform/DSSAT/")
 	setwd(path.to.extdata)
-	
 
 	lf <- list.files()
 	
 	f_all <- NULL
-	for (i in 1:length(lf)) {
-		
-		base <- lf[i]
 
+	f_all <- lapply(1:length(lf), function(i) {		
+		base <- lf[i]
 		if (file.exists(paste0(base,"/", base, ".OUT"))) {
 			a <- read_output(paste0(base,"/", base, ".OUT"))
 			d <- a[,c("XLAT","LONG","TRNO","TNAM","PDAT", "HDAT","CWAM","HWAH","CNAM","GNAM","NDCH","TMAXA",
@@ -31,23 +22,13 @@ merge_DSSAT_output <- function(country, useCaseName,Crop, AOI=FALSE,season=NULL)
 			b <- data.frame(b)
 			d$XLAT <- b$V15
 			d$base <- base
-				
-				# colnames(d) <- c('latitude','longitude','treatment.number','treatment.name','planting.date','harvesting.date','Total.aboveground.biomass(kg/ha)','WLY(kg/ha)',
-				#									'Total.aboveground.bio.N%(kg/ha)','GrainNMaturity(kg/ha)','crop.duration','Av.Tmax(°C)',
-				#									'Av.Tmin(°C)','A.Solar.rad(MJ/m2/d)','Total.Seasonal.Rainfall(mm)',
-				#									'Total.Seasonal.ETranspiration(mm)','Total.Seasonal.Soil.Evaporation(mm)')
-				
-			d$WUE <- d$HWAH / d$PRCP
-				
-			f_all <- rbind(f_all, d)
-		}
+				# colnames(d) <- c('latitude','longitude','treatment.number','treatment.name','planting.date','harvesting.date','Total.aboveground.biomass(kg/ha)','WLY(kg/ha)', 'Total.aboveground.bio.N%(kg/ha)','GrainNMaturity(kg/ha)','crop.duration','Av.Tmax(°C)', 'Av.Tmin(°C)','A.Solar.rad(MJ/m2/d)','Total.Seasonal.Rainfall(mm)',							'Total.Seasonal.ETranspiration(mm)','Total.Seasonal.Soil.Evaporation(mm)')
+			d$WUE <- d$HWAH / d$PRCP		
+			d
+		}})
+	f_all <- do.call(rbind, f_all)	
 		
-		
-	} 
-	if (AOI) {
-		saveRDS(f_all, file = paste0("/home/jovyan/agwise-potentialyield/dataops/potentialyield/Data/useCase_", country, "_",useCaseName, "/", Crop, "/result/DSSAT/AOI/useCase_", country, "_",useCaseName, "_", Crop,"_AOI_season_",season,".rds"))
-	} else {
-		saveRDS(f_all, file = paste0("/home/jovyan/agwise-potentialyield/dataops/potentialyield/Data/useCase_", country, "_",useCaseName, "/", Crop, "/result/DSSAT/useCase_", country, "_",useCaseName, "_", Crop,".rds")) 
-	}
+	fn <- = file.path(path, "/result/DSSAT/AOI.rds")	
+	saveRDS(f_all, fn)
 }
 
